7c6e5f739fbd9927cfc21eb93698eaa33282be2a
John <capoccigianni@gmail.com>
2024-06-08 21:24:17 +0300

    Test

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 mysql = MySQL(cursorclass=DictCursor)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'demetrio2'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'test'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
